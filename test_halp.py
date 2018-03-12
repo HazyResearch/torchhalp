a4a120af11977c853cf2901317d2bc516c84cb61
mleszczy <mleszczy@dawn16.stanford.edu>
2018-03-12 01:47:11 -0700

    Added tests sweeping mu and b

--- test_halp.py
+++ test_halp.py
@@ -38,18 +38,13 @@ def baseline_halp(x, y, w, lr, b, mu, T=1, K=1, calc_gradient=None):
     for k in range(K):
         # Recenter
         w = w + z # w is full precision
-        print "full grad w baseline", w
         g_k = calc_gradient(x, y, w, avg=True)
-        print "full grad baseline", g_k
         # Rescale
         s_k = float(np.linalg.norm(g_k)) / (mu * (2**(b-1) - 1))
         z = np.zeros(w.shape)
         for t in range(T):
             xi, yi = x[[t],:], y[t:t+1]
             z = z - (lr*(calc_gradient(xi, yi, w + z) - calc_gradient(xi, yi, w) + g_k))
-            # print "curr grad", calc_gradient(xi, yi, w + dequantize(z, s_k))
-            # print "prev grad", calc_gradient(xi, yi, w)
-            # print "prequant baseline", z
             z = quantize(z, b, s_k, biased=True)
             z = dequantize(z, s_k)
     return w + z
@@ -68,9 +63,6 @@ def pytorch_halp(x, y, w, lr, b, mu, T=1, K=1, n_features=None, n_classes=1):
 
     synth_dataset = SynthDataset(x, y)
     train_loader = torch.utils.data.DataLoader(synth_dataset)
-    print "length", len(train_loader), x.shape
-
-    print K, T
     halp_opt = HALP(model.parameters(), lr=lr, T=T, data_loader=train_loader, bits=b, mu=mu, biased=True)
 
     for k in range(K):
@@ -92,43 +84,46 @@ def pytorch_halp(x, y, w, lr, b, mu, T=1, K=1, n_features=None, n_classes=1):
 # Tests
 #========================================================================================
 
-@pytest.mark.parametrize("n_samples,n_features,lr,K",
+@pytest.mark.parametrize("n_samples,n_features,lr,K,b,mu",
 [
-    (1,   1,   1, 1),
-    (1,   4, 0.1, 1),
-    (1,   4, 0.1, 2),
-    (10,  4, 0.1, 1),
-    (10,  4, 0.1, 1),
-    (10, 10, 0.1, 1),
-    (10, 10, 0.5, 1),
-    (10, 10, 0.5, 10)
+    (1,   1,   1,  1,  8,   1),
+    (1,   4, 0.1,  1,  8,   1),
+    (1,   4, 0.1,  2,  8,   1),
+    (10,  4, 0.1,  1,  8,   1),
+    (10,  4, 0.1,  1,  8,   1),
+    (10, 10, 0.1,  1,  8,   1),
+    (10, 10, 0.5,  1,  8,   1),
+    (10, 10, 0.5, 10,  8,   1),
+    (10, 10, 0.5, 10,  8, 0.1),
+    (10, 10, 0.5, 10, 16, 0.1),
+    (5,  10, 0.5, 10, 16,   1)
 ])
-def test_linear_regress(n_samples, n_features, lr, K):
+def test_linear_regress(n_samples, n_features, lr, K, b, mu):
     x = np.random.rand(n_samples, n_features)
     y = np.random.uniform(0,1, size=(n_samples,))
     w = np.random.uniform(0, 0.1, (1, n_features))
-    b = 8
-    mu = 1
     np_value = baseline_halp(x, y, w, lr, b, mu, T=n_samples, K=K, calc_gradient=linear_grad)
     pytorch_value = pytorch_halp(x, y, w, lr, b, mu, T=n_samples, K=K, n_features=n_features)
     np.testing.assert_allclose(np_value, pytorch_value, rtol=1e-4)
 
-@pytest.mark.parametrize("n_samples,n_features,n_classes,lr,K",
+@pytest.mark.parametrize("n_samples,n_features,n_classes,lr,K,b,mu",
 [
-    (1, 1, 3,   1, 1),
-    (1, 4, 3, 0.1, 1),
-    (1, 4, 3, 0.1, 2),
-    (2, 4, 3, 0.1, 1),
-    (2, 4, 3, 0.5, 1),
-    (2, 4, 3, 0.5, 2),
-    (2, 4, 4, 0.5, 2)
+    (1, 1, 3,   1, 1, 8,   1),
+    (1, 4, 3, 0.1, 1, 8,   1),
+    (1, 4, 3, 0.1, 2, 8,   1),
+    (2, 4, 3, 0.1, 1, 8,   1),
+    (2, 4, 3, 0.5, 1, 8,   1),
+    (2, 4, 3, 0.5, 2, 8,   1),
+    (2, 4, 4, 0.5, 2, 8,   1),
+    (2, 4, 4, 0.5, 2, 8, 0.1),
+    (2, 4, 4, 0.5, 2, 16,0.1),
+    (2, 4, 4, 0.5, 2, 16,  1)
+
 ])
-def test_logistic_regress(n_samples, n_features, n_classes, lr, K):
+def test_logistic_regress(n_samples, n_features, n_classes, lr, K, b, mu):
     x = np.random.rand(n_samples, n_features)
     y = np.random.randint(0, n_classes, size=(n_samples,))
     w = np.random.uniform(0, 0.1, (n_classes, n_features))
-    b = 8
-    mu = 1
     np_value = baseline_halp(x, y, w, lr, b, mu, T=n_samples, K=K, calc_gradient=logistic_grad)
     pytorch_value = pytorch_halp(x, y, w, lr, b, mu, T=n_samples, K=K, n_features=n_features,
                                  n_classes=n_classes)
