1ad5bf995d4d908334346a97da870292e1aa017a
mleszczy <mleszczy@dawn10.stanford.edu>
2018-04-02 18:24:59 -0700

    Started adding more examples

--- test_quantize.py
+++ test_quantize.py
@@ -12,6 +12,10 @@ torch.cuda.FloatTensor.quantize_ = quantize.quantize_
 torch.cuda.FloatTensor.saturate_ = quantize.saturate_
 
 def check_saturation(m1, scale_factor, bits):
+	""" Returns True if the elements of the matrix m1 are within the
+	numbers representable by the specified scale_factor and bits,
+	False otherwise.
+	"""
 	min_val = -scale_factor*math.pow(2, bits-1)
 	max_val = scale_factor*(math.pow(2, bits-1) - 1)
 	m2 = m1.clone()
@@ -26,7 +30,8 @@ def check_saturation(m1, scale_factor, bits):
     (5e-9, 32)
 ])
 def test_quantize(scale_factor, bits):
-	m1 = torch.rand(100).mul(30).add(-15) # uniform [-15, 15]
+	# Create a matrix with 100 values randomly uniform within [-15, 15]
+	m1 = torch.rand(100).mul(30).add(-15)
 	m1.quantize_(scale_factor, bits)
 
 	# Test that quantized value is in it's range
@@ -35,7 +40,7 @@ def test_quantize(scale_factor, bits):
 	# Test that quantized value is representable
 	m2 = m1.clone()
 	for i in iter_indices(m2):
-		# Must be an integer in the "small" representation
+		# Must be an integer in the fixed-point representation
 		m2[i] = round(m2[i] / scale_factor) * scale_factor
 	np.testing.assert_allclose(m1.numpy(), m2.numpy(), rtol=1e-6)
 
